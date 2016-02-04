de42848ad06e6452e6181763ae1905df06b46dc8
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-04 20:38:17 -0300

    Fixing __init__

---
+++ ishell/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from ishell.profile import *  # noqa
--- ishell/config.py
+++ ishell/config.py
@@ -14,5 +14,8 @@ class CommandRegistry(object):
     def get_command(self, name, default=None):
         return self.__registry.get(name, default)
 
+    def get_all(self):
+        return self.__registry.copy()
+
 
 registry = CommandRegistry()
