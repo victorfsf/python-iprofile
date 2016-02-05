5a38ce25a216011433f52a8b1e1255c7dc4b661d
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-04 21:04:18 -0300

    Adding ishel/cli.py

---
+++ ishell/cli.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from ishell.config import registry
+from ishell.profile import *  # noqa
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+class IShell(object):
+
+    def __init__(self, argv, *args, **kwargs):
+        argv.pop(0)
+        if not argv:
+            raise AttributeError("No command given.")
+        command_name = argv.pop(0)
+        command = registry.get_command(command_name)
+        if not command:
+            raise AttributeError(
+                "The command '{}' does not exist.".format(command_name))
+        command(argv=argv, path=BASE_DIR)
+
+
+def main():
+    IShell(sys.argv)
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'ishell = ishell',
+            'ishell = ishell.cli:main',
         ],
     }
 )
