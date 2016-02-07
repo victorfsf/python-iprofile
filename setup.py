c81e32f365fbbcdad284ae49abedf5146ee9837b
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-07 09:51:42 -0300

    Adding coveralls requirement

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
