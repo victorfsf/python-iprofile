e049a06a92f9a8d27264c36703e2e6eef9d4bff3
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-09 20:01:19 -0300

    Update helpers.py

--- iprofile/texts/helpers.py
+++ iprofile/texts/helpers.py
@@ -9,7 +9,7 @@ HELP_DELETE = "Completely delete the profile and its files."
 HELP_INIT = "Create a new profile without saving it on IPython's path."
 HELP_SAVE = "Save changes to IPython's path."
 HELP_SHELL = "Open IPython with a given profile name (or the active profile)."
-HELP_LIST = "List all profiles."
+HELP_LIST = "Show a list of all profiles."
 
 HELP_NO_SYMLINKS = "Doesn't save the files as symlinks."
 HELP_PROFILE_DIR = "Set the IPython profile path."
