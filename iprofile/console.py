84cc4ba66508d2f16ea99739f9eefa2509876f08
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-10 10:52:08 -0300

    Updating version strings

--- iprofile/console.py
+++ iprofile/console.py
@@ -19,6 +19,6 @@ class IProfile(click.MultiCommand):
     cls=IProfile,
     help=texts.IPROFILE_READ_ME
 )
-@click.version_option(version='0.0.2', prog_name="IProfile")
+@click.version_option(version='0.0.3', prog_name="IProfile")
 def main():
     pass
