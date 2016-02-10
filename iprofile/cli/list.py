21b0c9ee017d14bf0525044df9a7f5cbe46e34e5
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-10 15:19:31 -0300

    Hotfix 0.1.1
    - Fixing profile listing

--- iprofile/cli/list.py
+++ iprofile/cli/list.py
@@ -5,8 +5,8 @@ from iprofile.core.decorators import icommand
 from iprofile.core.models import ICommand
 from iprofile.core.utils import get_ipython_path
 from iprofile.core.utils import get_profile_path
+from iprofile.core.utils import list_profiles
 import click
-import os
 
 
 @icommand(help=texts.HELP_LIST, short_help=texts.HELP_LIST)
@@ -19,11 +19,7 @@ class List(ICommand):
 
     def run(self, **options):
         try:
-            profiles = [
-                x for x in os.listdir('iprofiles')
-                if os.path.isdir(self.format(x)) and
-                'ipython_config.py' in os.listdir(self.format(x))
-            ]
+            profiles = list_profiles(self.project_path)
             qtd_profiles = len(profiles)
             if qtd_profiles == 0:
                 self.no_profiles()
@@ -54,6 +50,3 @@ class List(ICommand):
 
     def no_profiles(self):
         self.red(texts.ERROR_NO_PROFILES_TO_LIST)
-
-    def format(self, x):
-        return '{0}/{1}'.format(self.project_path, x)
