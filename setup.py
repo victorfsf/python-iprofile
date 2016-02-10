ab41d0c0fb064872af0adf9c454df4847d6a21c8
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-10 17:57:16 -0300

    Changing LICENSE on setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
