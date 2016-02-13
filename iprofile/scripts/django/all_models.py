d40d53ee6828320112b4895a291c6b8cd5e422d5
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-13 20:46:22 -0300

    Fixing code health

--- iprofile/scripts/django/all_models.py
+++ iprofile/scripts/django/all_models.py
@@ -4,6 +4,6 @@
 def load(__builtin__):
     from django.apps import apps
 
-    for app, models in apps.all_models.items():
+    for _, models in apps.all_models.items():
         for model in models.values():
             setattr(__builtin__, model.__name__, model)
