b45c6d25fed35807854a8fe7015f3714e5669e4a
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-14 16:42:23 -0300

    Fixing code health on all_models.py

--- iprofile/scripts/django/all_models.py
+++ iprofile/scripts/django/all_models.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 
-def load(__builtin__, exclude=[]):
+def load(__builtin__, exclude_app=None):
     from django.apps import apps
 
     for app, models in apps.all_models.items():
-        if app not in exclude:
+        if not (exclude_app and app in exclude_app):
             for model in models.values():
                 setattr(__builtin__, model.__name__, model)
