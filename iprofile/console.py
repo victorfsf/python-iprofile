a903929456e20e8ea79141543c018a3079f15c6f
Victor Ferraz <vfsf@cin.ufpe.br>
2016-02-22 09:20:27 -0300

    Updating LICENSE to MIT

--- iprofile/console.py
+++ iprofile/console.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) IPython Development Team.
-# Distributed under the terms of the Modified BSD License.
 
 from iprofile import texts
 from iprofile.core.config import registry
