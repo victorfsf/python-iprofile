ceb93029607204845d8994694c10bb338711575f
Victor Ferraz <vfsf@cin.ufpe.br>
2016-03-02 15:06:44 -0300

    Small changes on profiles models base

--- iprofile/profiles/models/base.py
+++ iprofile/profiles/models/base.py
@@ -21,15 +21,16 @@ class Profile(OSMixin):
         self.dirname = self.absjoin(
             settings.get('path'), name
         )
-        if not self.isdir(self.dirname):
-            pathlist = settings.get('pathlist')
-            if isinstance(pathlist, list):
-                for path in pathlist:
-                    if path and self.isdir(path):
-                        abspath = self.absuser(path)
-                        dirname = self.join(abspath, name)
-                        if self.isdir(dirname):
-                            self.dirname = dirname
+        if self.isdir(self.dirname):
+            return
+        pathlist = settings.get('pathlist')
+        if isinstance(pathlist, list):
+            for path in pathlist:
+                if path and self.isdir(path):
+                    abspath = self.absuser(path)
+                    dirname = self.join(abspath, name)
+                    if self.isdir(dirname):
+                        self.dirname = dirname
 
     def create(self):
         profile = IProfileCreate()
